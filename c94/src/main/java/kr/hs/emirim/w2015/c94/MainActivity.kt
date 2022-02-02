be99b3daf559428791498ec274f993c3f4ddb8df
Jyuri <jeon1310@gmail.com>
2022-02-02 12:45:51 +0900

    c94 - Toolbar

--- c94/src/main/java/kr/hs/emirim/w2015/c94/MainActivity.kt
+++ c94/src/main/java/kr/hs/emirim/w2015/c94/MainActivity.kt
@@ -1,11 +1,22 @@
 package kr.hs.emirim.w2015.c94
 
 import android.os.Bundle
+import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
+import kr.hs.emirim.w2015.c94.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return super.onCreateOptionsMenu(menu)
     }
 }
